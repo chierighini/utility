8412a12d3e713f63e6f69a7cee3156ab55805fc4
chierighini <chierighini.andre@gmail.com>
2021-06-14 19:48:48 -0300

    first commit

---
+++ vm_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script for setting up pre-made virtual machines to my liking
+# will probably increase as time goes
+apt update
+apt upgrade
+setxkbmap -model abnt2 -layout br
+touch ~/.hushlogin
