d294c657df3374b3b8b144a8fa40cc29ea9ae311
chierighini <chierighini.andre@gmail.com>
2022-06-03 21:31:22 -0300

    create sub dirs

--- vm_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# script for setting up pre-made virtual machines to my liking
-# will probably increase as time goes
-apt update
-apt upgrade
-apt install seclists
-setxkbmap -model abnt2 -layout br
-touch ~/.hushlogin
