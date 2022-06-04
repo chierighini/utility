a1095ca66ec79fd3d00b08bd88ff23722c2d4730
chierighini <chierighini.andre@gmail.com>
2022-06-03 21:22:04 -0300

    Add AUR_update script

---
+++ AUR_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for dir in ~/AUR/*/
+do
+    cd ${dir}
+    dir=${dir%*/}
+    echo "${dir##*/}"
+    if  git pull | grep 'Already up to date'
+    then
+        echo OK
+    else
+        makepkg -si
+    fi
+    
+done
